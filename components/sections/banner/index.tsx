14ef1052e7c7ae1afada8b873bcd53296153d7ab
ntabucejo <ntabucejo@gmail.com>
2022-07-09 14:00:15 +0800

    Update font size

--- components/sections/banner/index.tsx
+++ components/sections/banner/index.tsx
@@ -12,7 +12,7 @@ const Banner: FunctionComponent<Props> = ({ title, description, link }) => {
       <h1 className="mb-2 text-lg font-semibold underline-offset-2 hover:underline">
         <a href={link}>{title}</a>
       </h1>
-      <p>{description}</p>
+      <p className="text-sm">{description}</p>
     </section>
   );
 };
